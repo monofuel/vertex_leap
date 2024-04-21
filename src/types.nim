089ee10ffb1d843a9b798ffd15f033e244a7642d
monofuel <monofuel@japura.net>
2024-04-21 14:35:41 -0400

    more types

--- src/types.nim
+++ src/types.nim
@@ -1,6 +1,30 @@
-import curly, jsony, std/[locks, times, options]
+import curly, std/[locks, json, times, options]
 
 type
+  SafetyCategory* = enum
+    HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    HARM_CATEGORY_HATE_SPEECH,
+    HARM_CATEGORY_HARASSMENT,
+    HARM_CATEGORY_DANGEROUS_CONTENT
+  SafetyThreshold* = enum
+    BLOCK_NONE,
+    BLOCK_LOW_AND_ABOVE,
+    BLOCK_MED_AND_ABOVE,
+    BLOCK_ONLY_HIGH
+  SafetyProbability* = enum
+    HARM_PROBABILITY_UNSPECIFIED,
+    NEGLIGIBLE,
+    LOW,
+    MEDIUM,
+    HIGH
+  GeminiProFinishReason* = enum
+    FINISH_REASON_UNSPECIFIED,
+    FINISH_REASON_STOP,
+    FINISH_REASON_MAX_TOKENS,
+    FINISH_REASON_SAFETY,
+    FINISH_REASON_RECITATION,
+    FINISH_REASON_OTHER
+
   GCPCredentials* = ref object
     projectId*: string
     privateKey*: string
@@ -18,13 +42,29 @@ type
     authToken*: string # ephemeral auth token, valid for 1 hour
 
   # Gemini-Pro types
+  GeminiProInlineData* = ref object
+    mimeType*: string
+    data*: string
+  GeminiProFileData* = ref object
+    mimeType*: string
+    uri*: string
+  VideoOffset* = ref object
+    seconds*: int
+    nanos*: int
+  GeminiProVideoMetadata* = ref object
+    startOffset*: VideoOffset
+    endOffset*: VideoOffset
   GeminiProTextPart* = ref object
-    text*: string
+    # These 3 fields form a union, only one should be set
+    text*: Option[string]
+    inlineData*: Option[GeminiProInlineData]
+    fileData*: Option[GeminiProFileData]
   GeminiProSystemInstruction* = ref object
+    role*: string # this field is ignored
     parts*: seq[GeminiProTextPart]
   GeminiProSafetySettings* = ref object
-    category*: string
-    threshold*: string
+    category*: SafetyCategory
+    threshold*: SafetyThreshold
   GeminiProGenerationConfig* = ref object
     temperature*: float
     topP*: float
@@ -32,17 +72,25 @@ type
     candidateCount*: Option[int]
     maxOutputTokens*: Option[int]
     stopSequences*: Option[seq[string]]
+    responseMimeType*: Option[string] # either "text/plain" or "application/json"
   GeminiProContents* = ref object
     role*: string
     parts*: seq[GeminiProTextPart]
+  GeminiProFunction* = ref object
+    name*: string
+    description*: string
+    parameters*: JsonNode # OpenAPI schema
+  GeminiProTool* = ref object
+    functionDescription*: seq[GeminiProFunction]
   GeminiProRequest* = ref object
     contents*: seq[GeminiProContents]
     systemInstruction*: Option[GeminiProSystemInstruction]
+    tools*: Option[seq[GeminiProTool]]
     safetySettings*: seq[GeminiProSafetySettings]
     generationConfig*: GeminiProGenerationConfig
   GeminiProSafetyRating* = ref object
-    category*: string # TODO enum
-    probability*: string # TODO enum
+    category*: SafetyCategory
+    probability*: SafetyProbability
     blocked*: bool
   GeminiProCitationDate* = ref object
     year*, month*, day*: int
@@ -56,7 +104,7 @@ type
     parts*: seq[GeminiProTextPart]
   GeminiProCandidate* = ref object
     content*: GeminiProCandidateContent
-    finishReason*: string # TODO enum
+    finishReason*: GeminiProFinishReason
     safetyRatings*: seq[GeminiProSafetyRating]
     citationMetadata*: GeminiProCitationMetadata
   GeminiProUsageMetadata* = ref object
--- tests/test_gemini_pro.nim
+++ tests/test_gemini_pro.nim
@@ -5,6 +5,10 @@ import vertex_leap, jsony, std/[unittest, options, os]
 # gemini-1.0-pro-vision-001
 # gemini-1.5-pro-preview-0409
 
+const
+  Gemini_1_5_pro = "gemini-1.5-pro-preview-0409"
+  Gemini_1_0_pro_002 = "gemini-1.0-pro-002"
+  Gemini_1_0_pro_vision_001 = "gemini-1.0-pro-vision_001"
 
 suite "gemini pro":
   var vertexai: VertexAIAPI
@@ -24,5 +28,10 @@ suite "gemini pro":
   suite "1.0-pro-002":
     test "get":
       let prompt = "Please talk like a pirate. you are Longbeard the Llama."
-      let resp = vertexai.geminiProGenerate("gemini-1.0-pro-002", prompt)
+      let resp = vertexai.geminiProGenerate(Gemini_1_0_pro_002, prompt)
+      echo resp
+  suite "1.5-pro":
+    test "get":
+      let prompt = "Please talk like a pirate. you are Longbeard the Llama."
+      let resp = vertexai.geminiProGenerate(Gemini_1_5_pro, prompt)
       echo resp
